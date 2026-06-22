82aafdc3f361904724a808efd8e458d3921af28c
Cong Ma <macong911@gmail.com>
2026-06-22 23:17:35 +0800

    Initial software portfolio site

---
+++ script.js
@@ -0,0 +1,16 @@
+const filterButtons = document.querySelectorAll(".filter-button");
+const catalogItems = document.querySelectorAll(".catalog-item");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.dataset.filter;
+
+    filterButtons.forEach((item) => item.classList.remove("is-active"));
+    button.classList.add("is-active");
+
+    catalogItems.forEach((item) => {
+      const shouldShow = filter === "all" || item.dataset.category === filter;
+      item.classList.toggle("is-hidden", !shouldShow);
+    });
+  });
+});
